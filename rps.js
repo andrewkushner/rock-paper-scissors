f34cec2accabddd52ceb9777dcadffcf63378c22
Andrew K <andrewrkushner@gmail.com>
2023-09-08 14:40:20 -0400

    Initial creation and working on function for computer selection.

---
+++ rps.js
@@ -0,0 +1,8 @@
+const computerSelection = getComputerChoice();
+
+function getComputerChoice (){
+    let compChoice = Math.floor(Math.random() * 2);
+    return
+}
+
+console.log(getComputerChoice());
