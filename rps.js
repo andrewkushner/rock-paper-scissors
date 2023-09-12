435d62650b96cc40bd36cdd988fddd28df786aaf
Andrew K <andrewrkushner@gmail.com>
2023-09-12 14:19:30 -0400

    Switch/case function created for comparing user input to computer input. Unable to get user input using prompt but was able to validate readline is working. Added commentary.

--- rps.js
+++ rps.js
@@ -1,8 +1,63 @@
-const computerSelection = getComputerChoice();
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
 function getComputerChoice (){
-    let compChoice = Math.floor(Math.random() * 2);
-    return
+        var moveOptions = ['rock', 'paper', 'scissors'],
+        randNum = Math.floor(Math.random() * moveOptions.length),
+        compMove = moveOptions[randNum];
+    return compMove;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection){
+        case 'rock':
+            if(computerSelection === 'rock') {
+            return 'draw';
+            }
+            else if (computerSelection === 'paper') {
+                return 'You Lose.';
+            }
+            else {
+                return 'You win.';
+            }
+            break;
+        
+        case 'paper':
+            if(computerSelection === 'rock') {
+            return 'You win';
+            }
+            else if (computerSelection === 'paper') {
+                return 'Draw.';
+            }
+            else {
+                return 'You lose.';
+            }
+            break;
+
+        case 'scissors':
+            if(computerSelection === 'rock') {
+            return 'You lose.';
+            }
+            else if (computerSelection === 'paper') {
+                return 'You win.';
+            }
+            else {
+                return 'Draw.';
+            }
+            break;
+    }
+}
+
+// const playerSelection = readline.question();
+var computerSelection = getComputerChoice();
+// var playerSelection = '';
+
+// console.log(playRound(playerSelection, computerSelection));
+
+readline.question('Rock, Paper or Scissors? ', (playerSelection) => {
+    console.log(playRound(playerSelection, computerSelection));
+    readline.close();
+  });
