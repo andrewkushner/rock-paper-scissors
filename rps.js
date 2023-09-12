d0f3208baeec4bb9ebf536db5342f120a230b87a
Andrew K <andrewrkushner@gmail.com>
2023-09-12 16:45:10 -0400

    Updated readline to include promises, and await for user input. Added a check to validate user input is an expected value. Need to add additional comments and optimization.

--- rps.js
+++ rps.js
@@ -1,12 +1,20 @@
 // Readline is required to read user input.
 // Attempted to use prompt but unable to test successfully.
 // Seems to be related to an outdated library and testing on a mac.
-
-const readline = require('readline').createInterface({
+const readline = require("readline/promises").createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   });
 
+  
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+
+var userScore = 0;
+var compScore = 0;
+var drawScore = 0;
  
 function getComputerChoice (){
         var moveOptions = ['rock', 'paper', 'scissors'],
@@ -26,42 +34,92 @@ function playRound(playerSelection, computerSelection){
     switch (playerSelection){
         case 'rock':
             if(computerSelection === 'rock') {
-            return 'Draw.';
+                drawScore += 1;
+                return 'Draw.';
             }
             else if (computerSelection === 'paper') {
+                compScore += 1;
                 return 'You lose.';
             }
             else {
+                userScore += 1;
                 return 'You win.';
             }
             break;
         
         case 'paper':
             if(computerSelection === 'rock') {
-            return 'You win';
+                userScore += 1;
+                return 'You win';
             }
             else if (computerSelection === 'paper') {
+                drawScore += 1;
                 return 'Draw.';
             }
             else {
+                compScore += 1;
                 return 'You lose.';
             }
             break;
 
         case 'scissors':
             if(computerSelection === 'rock') {
-            return 'You lose.';
+                compScore += 1;
+                return 'You lose.';
             }
             else if (computerSelection === 'paper') {
+                userScore += 1;
                 return 'You win.';
             }
             else {
+                drawScore += 1;
                 return 'Draw.';
             }
             break;
     }
 }
 
+function finalScore(userScore, compScore){
+    if (userScore > compScore){
+        return console.log("You win! Your score: " + userScore + ". Computer Score: " + compScore + ". Tie games: " + drawScore);
+    }
+    else if (userScore < compScore){
+        return console.log("You lost. Your score: " + userScore + ". Computer Score: " + compScore + ". Tie games: " + drawScore);
+    }
+    else {
+        return console.log("It's a tie.");
+    }
+}
+
+async function game() {
+    for (let i = 0; i < 5; i++) {
+      var answers = ['rock', 'paper', 'scissors'];
+      var computerSelection = getComputerChoice();
+      var playerSelection = await readline.question(`Rock, Paper or Scissors? `);
+     
+      if (answers.includes(playerSelection.toLowerCase())){
+        console.log(playRound(playerSelection, computerSelection));
+      }
+      else{
+        playerSelection = await readline.question(`\nPlease enter a valid selection: `);
+      }
+    }
+    readline.close();
+    console.log(finalScore(userScore,compScore));
+}
+
+game();
+
+
+
+
+// // Readline question asks a question, storing user input within the variable, then use that variable in a function.
+// readline.question('Rock, Paper or Scissors? ', (playerSelection) => {
+//     console.log(playRound(playerSelection, computerSelection));
+//     readline.close();
+//   });
+
+// // Original code
 // function game(){
 //     var computerSelection = getComputerChoice();
 //         for (let i = 0; i < 5; i++) {
@@ -73,23 +131,20 @@ function playRound(playerSelection, computerSelection){
 //         }
 // }
 
-function game(){
-    let i = 0;
-    do{
-        i += 1;
-        var computerSelection = getComputerChoice();
-        // Readline question asks a question, storing user input within the variable, then use that variable in a function.
-        readline.question('Rock, Paper or Scissors? ', (playerSelection) => {
-            console.log(playRound(playerSelection, computerSelection));
-            readline.close();
-        });  
-    } while (i < 5);
-}
 
-game();
+// Attempting to fix the loop - corrected the var computerSelection order as it was stuck on one input rather than updating each play.
+// function game(){
+//         var computerSelection = getComputerChoice();
+//         // Readline question asks a question, storing user input within the variable, then use that variable in a function.
+//         readline.question('Rock, Paper or Scissors? ', (playerSelection) => {
+//             console.log(playRound(playerSelection, computerSelection));
+//             readline.close();
+//         });  
+// }
 
-// // Readline question asks a question, storing user input within the variable, then use that variable in a function.
-// readline.question('Rock, Paper or Scissors? ', (playerSelection) => {
-//     console.log(playRound(playerSelection, computerSelection));
-//     readline.close();
-//   });
+// var i = 0;
+// while (i < 5){
+//         game();
+//         i++;
+//         console.log(i);
+// }
